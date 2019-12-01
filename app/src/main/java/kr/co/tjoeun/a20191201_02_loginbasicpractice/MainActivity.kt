fa89ac8827f8bad9d07a7cdc23f36cd15f60d130
조경진 <kj_cho@idealidea.co.kr>
2019-12-01 12:29:53 +0900

    로그인 기능 구현  - admin / pw123 입력되면 관리자  - 그외에는 일반사용자 토스트 출력

--- app/src/main/java/kr/co/tjoeun/a20191201_02_loginbasicpractice/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/a20191201_02_loginbasicpractice/MainActivity.kt
@@ -2,11 +2,37 @@ package kr.co.tjoeun.a20191201_02_loginbasicpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+            var inputId = idEdt.text.toString()
+            var inputPw = pwEdt.text.toString()
+
+            if (inputId == "admin" && inputPw == "pw123") {
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "일반 사용자 입니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
